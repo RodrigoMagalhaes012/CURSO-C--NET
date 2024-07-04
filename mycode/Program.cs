4d4cd0dd53f8feb1be78fc0af410be83d749c49c
Rodrigo Amorim <rodrigom.21amorim@gmail.com>
2024-07-04 18:34:11 -0300

    first commit

---
+++ mycode/Program.cs
@@ -0,0 +1,30 @@
+﻿// using System;
+
+// namespace Application
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World");
+//         }
+//     }
+// }
+
+using FluentColorConsole;
+
+Console.WriteLine("Hello World");
+
+//somente um arquivo pode usar essa instrução de nivel superior
+// void ShowMessage2()
+// {
+//     Console.WriteLine("Hello World2");
+// }
+
+// ShowMessage2();
+
+// var ShowMessage = new ShowMessage();
+// ShowMessage.WriteLine();
+
+var textLine = ColorConsole.WithBlueText;
+textLine.WriteLine("My text blue");
