9921f95651b25e3b5cb02485c946993c46793e1f
Rodrigo Amorim <rodrigom.21amorim@gmail.com>
2024-11-09 13:20:41 -0300

    Criação de variaveis de ambiente

--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
 var app = builder.Build();
+var configuration = builder.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapGet("/", () => "Hello World 2");
 
@@ -55,12 +57,23 @@ app.MapDelete("/products/{code}", ([FromRoute] string code) =>
     return Results.Ok(productSaved);
 });
 
+app.MapGet("/configuration/database", (IConfiguration configuration) =>
+{
+    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
 
 app.Run();
 
 public static class ProductRepository
 {
     private static List<Product> Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration)
+    {
+        var products = configuration.GetSection("products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product)
     {
